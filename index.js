038fd9a131e53130224ddaf8e401dfd1bde01086
Debkumar04 <shitdebkumar2003@gmail.com>
2023-11-02 16:58:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require('http')
+
+const server= http.createServer(callBack);
+
+let a=[];
+
+function callBack(req , res){
+
+    // console.log(req.url)
+    // console.log(req.method)
+
+    if(req.method=='GET')
+    {
+        
+    }
+    else if(req.method=='POST')
+    {
+        a.push(1);
+        
+    }
+    else if(req.method=='PUT')
+    {
+        a[1]=2;
+    }
+    else if(req.method=='DELETE')
+    {
+        a.pop();
+    }
+
+    res.write(JSON.stringify(a));
+    res.end();
+}
+
+
+server.listen(7000)
